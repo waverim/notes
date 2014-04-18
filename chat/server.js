eb4a2cbfff10ec29eee5c6cacbcf0ba43d572709
Waverim <waverimax@gmail.com>
2014-04-18 21:17:04 +0800

    the first version of chat.io

---
+++ chat/server.js
@@ -0,0 +1,23 @@
+
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(3000); 
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+    });
+
+app.use('/public', require('express').static(__dirname + '/public'));
+
+io.sockets.on('connection', function(socket) {
+	socket.on('join', function (name) {
+		socket.nickname = name;
+		socket.broadcast.emit('announcement', name + ' join the chat.');
+	    });
+
+	socket.on('text', function (msg) {
+		socket.broadcast.emit('text', socket.nickname, msg);
+	    });
+});
